4a4b9bcace862c402cd5a9d81eb2bbb608f12c49
Cifumo <146169339+cifumo@users.noreply.github.com>
2024-05-06 16:45:52 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'https://api.reysekha.xyz';
+var url = 'https://emiliawebsite-a47c0698325a.herokuapp.com';
 try {
-if(url == 'https://api.reysekha.xyz') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'https://emiliawebsite-a47c0698325a.herokuapp.com') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
